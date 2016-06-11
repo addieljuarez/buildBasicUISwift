b15a5937b1a9a5c25f7eb577c3cdf89b8c81a8a3
Addiel <addieljuarez@gmail.com>
2016-06-10 21:50:09 -0500

    parte 2

--- buildBasicUISwift/ViewController.swift
+++ buildBasicUISwift/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameNameLabel: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        nameTextField.text = textField.text
+    }
+    
+    @IBAction func setDefaultlabelText(sender: UIButton) {
+        nameNameLabel.text = "default text"
+    }
 
 }
 
